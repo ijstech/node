02d2825b6e5b15bd63b7793290609762e6131c24
instajs <ycwong@instajs.com>
2021-03-17 19:22:14 +0800

    bump version to 0.1.2

--- index.js
+++ index.js
@@ -18,7 +18,26 @@ class AppServer {
             else
                 return console.log('Server configuration not defined!')
         }        
-        this.options = options;        
+        this.app = new Koa();        
+        this.options = options;
+        this.ciphers = options.ciphers || [
+            "ECDHE-RSA-AES256-SHA384",
+            "DHE-RSA-AES256-SHA384",
+            "ECDHE-RSA-AES256-SHA256",
+            "DHE-RSA-AES256-SHA256",
+            "ECDHE-RSA-AES128-SHA256",
+            "DHE-RSA-AES128-SHA256",
+            "HIGH",
+            "!aNULL",
+            "!eNULL",
+            "!EXPORT",
+            "!DES",
+            "!RC4",
+            "!MD5",
+            "!PSK",
+            "!SRP",
+            "!CAMELLIA"
+        ].join(':');  
     }    
     getCert(domain){            
         let self = this;
@@ -30,6 +49,8 @@ class AppServer {
             try{                
                 if (Fs.existsSync(Path.resolve(RootPath, certPath, domain, 'privkey.pem'))){                    
                     var crt = Tls.createSecureContext({
+                        ciphers: self.ciphers,
+                        honorCipherOrder: true,
                         key:  Fs.readFileSync(Path.resolve(RootPath, certPath, domain, 'privkey.pem')),
                         cert: Fs.readFileSync(Path.resolve(RootPath, certPath, domain, 'fullchain.pem'))
                     })	
@@ -38,6 +59,8 @@ class AppServer {
                 }
                 else if (Fs.existsSync(Path.resolve(RootPath, certPath, domain, 'ca_bundle.crt'))){                    
                     var crt = Tls.createSecureContext({
+                        ciphers: self.ciphers,
+                        honorCipherOrder: true,
                         key:  Fs.readFileSync(Path.resolve(RootPath, certPath, domain, 'private.key')),
                         cert: Fs.readFileSync(Path.resolve(RootPath, certPath, domain, 'ca_bundle.crt'))
                     })	
@@ -67,8 +90,23 @@ class AppServer {
     start(){
         if (this.running)
             return;
-        this.running = true;        
-        this.app = new Koa();        
+        this.running = true;     
+        // this.app.use(async function(ctx, next){    
+        //     return new Promise(function(resolve, reject){
+        //         if (ctx.request.type == 'multipart/form-data'){
+        //             var data = "";
+        //             ctx.req.on("data", chunk => {
+        //                 data += chunk
+        //             });
+        //             ctx.req.on("end", async () => {
+        //                 ctx.raw_data = data;                
+        //                 await next();
+        //             });            
+        //         }
+        //         else            
+        //             await next();
+        //     })                                
+        // })   
         this.app.use(BodyParser());
         let middlewares = [];   
         if (this.options.plugin){         
@@ -77,41 +115,19 @@ class AppServer {
                 let p = require(n);                
                 if (typeof(p._middleware) == 'function')
                     middlewares.push(p._middleware);
-                if (typeof(p._init) == 'function')
+                if (typeof(p._init) == 'function'){
                     p._init(opt, function(middleware){
                         if (middleware && middlewares.indexOf(middleware) < 0)
                             middlewares.push(middleware);
                     });
-            }
-        }
+                };
+            };
+        };   
         if (this.options.port || this.options.securePort){
             this.ssl = {};
-            for (let v in this.options.middleware){
-                let opt = this.options.middleware[v];
-                let p = require(v);
-                
-                if (typeof(p._middleware) == 'function'){
-                    if (typeof(p._init) == 'function')
-                        p._init(opt);
-                    if (middlewares.indexOf(p._middleware) < 0)
-                        this.app.use(p._middleware);
-                }
-                else if (typeof(p) == 'function'){
-                    this.app.use(p(opt));
-                }                                    
-            }
-            for (let i = 0; i < middlewares.length; i ++){
-                this.app.use(middlewares[i]);
-            }
-            this.app.use(ctx => {
-                ctx.status = 404;
-                ctx.body = page404;
-            });
-    
             let self = this;
             if (this.options.port){                        
                 let http = require('http');
-                this.httpConnections = new Set();
                 this.http = http.createServer(this.app.callback()).listen(this.options.port,'0.0.0.0');            
                 console.log(`http server is running at ${this.options.port}`);
             };
@@ -122,14 +138,36 @@ class AppServer {
                         var crt = await self.getCert(domain);                    
                         callback(null, crt);
                     }
-                }
+                };
                 this.https = https.createServer(SNIOptions, this.app.callback()).listen(this.options.securePort);            
                 console.log(`https server is running at ${this.options.securePort}`);
             }; 
-        }                       
-    }
+
+            for (let v in this.options.middleware){
+                let opt = this.options.middleware[v];
+                let p = require(v);
+                
+                if (typeof(p._middleware) == 'function'){
+                    if (typeof(p._init) == 'function')
+                        p._init(opt, this);
+                    if (middlewares.indexOf(p._middleware) < 0)
+                        this.app.use(p._middleware);
+                }
+                else if (typeof(p) == 'function'){
+                    this.app.use(p(opt));
+                };
+            };
+            for (let i = 0; i < middlewares.length; i ++){
+                this.app.use(middlewares[i]);
+            };
+            this.app.use(ctx => {
+                ctx.status = 404;
+                ctx.body = page404;
+            });            
+        };
+    };
     use(middleware){                
         this.app.use(middleware);
-    }
-}
+    };
+};
 module.exports = AppServer;
