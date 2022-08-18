cf212d658406feebbbcf12bc6c0586fca50b965d
instajs <ycwong@instajs.com>
2022-08-18 16:32:35 +0800

    delete form package

--- packages/form/test/test.ts
+++
@@ -1,3 +0,0 @@
-import Config from './config.js';
-
-console.dir(Config);
--- packages/form/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-/*!-----------------------------------------------------------
-* Copyright (c) IJS Technologies. All rights reserved.
-* Released under dual AGPLv3/commercial license
-* https://ijs.network
-*-----------------------------------------------------------*/
-import { Context } from 'koa';
-export declare function resolveFilePath(rootPaths: string[], filePath: string): string;
-declare function route(ctx: Context, options: any): Promise<void>;
-export default route;
