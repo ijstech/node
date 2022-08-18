41a35cf68c72ae612226e36f88ca906c71075286
instajs <ycwong@instajs.com>
2022-08-18 19:02:43 +0800

    export getJobQueue function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export {AppServer, IAppServerOptions} from '@ijstech/app';
 export {Router, Worker} from '@ijstech/plugin';
 export {Compiler, PluginCompiler, WalletPluginCompiler} from '@ijstech/tsc';
+export {getJobQueue} from '@ijstech/queue';
 export {IRouterPlugin, IWorkerPlugin, IRequiredPlugins, ResponseType, IRouterRequest, IRouterResponse, ISession} from '@ijstech/types';
