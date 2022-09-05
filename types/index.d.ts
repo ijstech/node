cff09d35df01b2903051db9c37fc290b606761bd
instajs <ycwong@instajs.com>
2022-09-05 16:29:08 +0800

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,5 @@
 export { AppServer, IAppServerOptions } from '@ijstech/app';
-export { Router, Worker, PluginCompiler, ResponseType, IRouterRequest, IRouterResponse, ISession, IRequiredPlugins, IRouterPlugin, IRouterPluginOptions, IWorkerPlugin, IWorkerPluginOptions } from '@ijstech/plugin';
+export { Router, Worker, ResponseType, IRouterRequest, IRouterResponse, ISession, IRequiredPlugins, IRouterPlugin, IRouterPluginOptions, IWorkerPlugin, IWorkerPluginOptions } from '@ijstech/plugin';
 export { Compiler, WalletPluginCompiler } from '@ijstech/tsc';
 export { getJobQueue, JobQueue, IJobQueueOptions } from '@ijstech/queue';
 export { Scheduler } from '@ijstech/schedule';
