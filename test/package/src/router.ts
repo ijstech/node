d5a68651c243212a3bf1479522105b70a36914a9
instajs <ycwong@instajs.com>
2022-09-20 12:15:32 +0800

    delete test package

--- test/package/src/router.ts
+++
@@ -1,8 +0,0 @@
-import {IRouterPlugin, IRouterRequest, IRouterResponse, ISession} from '@ijstech/plugin';
-
-export default class HelloWorld implements IRouterPlugin{    
-    async route(session: ISession, request: IRouterRequest, response: IRouterResponse): Promise<boolean>{
-        response.end('hello!');
-        return true;
-    }
-}
--- test/package/src/worker.ts
+++
@@ -1,9 +0,0 @@
-import {IWorkerPlugin, ISession} from '@ijstech/plugin';
-
-export default class HelloWorld implements IWorkerPlugin{    
-    async process(session: ISession, data: any): Promise<any> {        
-        console.dir('Worker !')
-        console.dir(data)
-        return true;
-    }
-}
