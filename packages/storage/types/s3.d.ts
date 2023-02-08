ab1766334d27e938a512be64df7a588a4539261d
instajs <ycwong@instajs.com>
2023-02-08 16:58:26 +0800

    Update s3.d.ts

--- packages/storage/types/s3.d.ts
+++ packages/storage/types/s3.d.ts
@@ -1,4 +1,4 @@
-import { CompleteMultipartUploadCommandOutput, DeleteObjectCommandOutput, ListObjectsV2CommandOutput, PutObjectCommandOutput, HeadObjectCommandOutput } from "@aws-sdk/client-s3";
+import { CompleteMultipartUploadCommandOutput, ListObjectsV2CommandOutput, PutObjectCommandOutput, HeadObjectCommandOutput } from "@aws-sdk/client-s3";
 import { ICidInfo } from '@ijstech/ipfs';
 export interface IS3Options {
     region?: string;
@@ -11,7 +11,8 @@ export declare class S3 {
     private s3;
     private options;
     constructor(options: IS3Options);
-    deleteObject(key: string): Promise<DeleteObjectCommandOutput>;
+    copyObject(fromKey: string, toKey: string): Promise<boolean>;
+    deleteObject(key: string): Promise<boolean>;
     hasObject(key: string): Promise<boolean>;
     headObject(key: string): Promise<HeadObjectCommandOutput>;
     syncFiles(sourceDir: string, targetDir: string, items: ICidInfo[]): Promise<void>;
@@ -22,6 +23,7 @@ export declare class S3 {
     }): Promise<ListObjectsV2CommandOutput>;
     getObject(key: string): Promise<string>;
     getObjectSignedUrl(key: string, expiresInSeconds?: number): Promise<string>;
+    moveObject(fromKey: string, toKey: string): Promise<boolean>;
     putObject(key: string, content: string): Promise<PutObjectCommandOutput>;
     putObjectSignedUrl(key: string, expiresInSeconds?: number): Promise<string>;
     putObjectFrom(key: string, filePath: string, progressCallback?: any): Promise<CompleteMultipartUploadCommandOutput>;
