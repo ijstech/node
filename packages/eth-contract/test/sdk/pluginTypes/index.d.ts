5a0475959ed88237440682f8856986a3203eb57b
benho-ijs <benho@instajs.com>
2023-03-30 12:05:38 +0800

    rebuild test contracts

---
+++ packages/eth-contract/test/sdk/pluginTypes/index.d.ts
@@ -0,0 +1,132 @@
+/// <amd-module name="@demo/sdk/contracts/ERC20.json.ts" />
+declare module "@demo/sdk/contracts/ERC20.json.ts" {
+    const _default: {
+        abi: ({
+            anonymous: boolean;
+            inputs: {
+                indexed: boolean;
+                internalType: string;
+                name: string;
+                type: string;
+            }[];
+            name: string;
+            type: string;
+            outputs?: undefined;
+            stateMutability?: undefined;
+        } | {
+            inputs: {
+                internalType: string;
+                name: string;
+                type: string;
+            }[];
+            name: string;
+            outputs: {
+                internalType: string;
+                name: string;
+                type: string;
+            }[];
+            stateMutability: string;
+            type: string;
+            anonymous?: undefined;
+        })[];
+        bytecode: string;
+    };
+    export default _default;
+}
+/// <amd-module name="@demo/sdk/contracts/ERC20.ts" />
+declare module "@demo/sdk/contracts/ERC20.ts" {
+    import { IWallet, Contract as _Contract, TransactionReceipt, BigNumber, Event, TransactionOptions } from "@ijstech/eth-contract";
+    export interface IAllowanceParams {
+        param1: string;
+        param2: string;
+    }
+    export interface IApproveParams {
+        spender: string;
+        amount: number | BigNumber;
+    }
+    export interface ITransferParams {
+        recipient: string;
+        amount: number | BigNumber;
+    }
+    export interface ITransferFromParams {
+        sender: string;
+        recipient: string;
+        amount: number | BigNumber;
+    }
+    export class ERC20 extends _Contract {
+        static _abi: any;
+        constructor(wallet: IWallet, address?: string);
+        deploy(options?: TransactionOptions): Promise<string>;
+        parseApprovalEvent(receipt: TransactionReceipt): ERC20.ApprovalEvent[];
+        decodeApprovalEvent(event: Event): ERC20.ApprovalEvent;
+        parseTransferEvent(receipt: TransactionReceipt): ERC20.TransferEvent[];
+        decodeTransferEvent(event: Event): ERC20.TransferEvent;
+        allowance: {
+            (params: IAllowanceParams, options?: TransactionOptions): Promise<BigNumber>;
+        };
+        approve: {
+            (params: IApproveParams, options?: TransactionOptions): Promise<TransactionReceipt>;
+            call: (params: IApproveParams, options?: TransactionOptions) => Promise<boolean>;
+            txData: (params: IApproveParams, options?: TransactionOptions) => Promise<string>;
+        };
+        balanceOf: {
+            (param1: string, options?: TransactionOptions): Promise<BigNumber>;
+        };
+        burn: {
+            (amount: number | BigNumber, options?: TransactionOptions): Promise<TransactionReceipt>;
+            call: (amount: number | BigNumber, options?: TransactionOptions) => Promise<void>;
+            txData: (amount: number | BigNumber, options?: TransactionOptions) => Promise<string>;
+        };
+        decimals: {
+            (options?: TransactionOptions): Promise<BigNumber>;
+        };
+        mint: {
+            (amount: number | BigNumber, options?: TransactionOptions): Promise<TransactionReceipt>;
+            call: (amount: number | BigNumber, options?: TransactionOptions) => Promise<void>;
+            txData: (amount: number | BigNumber, options?: TransactionOptions) => Promise<string>;
+        };
+        name: {
+            (options?: TransactionOptions): Promise<string>;
+        };
+        symbol: {
+            (options?: TransactionOptions): Promise<string>;
+        };
+        totalSupply: {
+            (options?: TransactionOptions): Promise<BigNumber>;
+        };
+        transfer: {
+            (params: ITransferParams, options?: TransactionOptions): Promise<TransactionReceipt>;
+            call: (params: ITransferParams, options?: TransactionOptions) => Promise<boolean>;
+            txData: (params: ITransferParams, options?: TransactionOptions) => Promise<string>;
+        };
+        transferFrom: {
+            (params: ITransferFromParams, options?: TransactionOptions): Promise<TransactionReceipt>;
+            call: (params: ITransferFromParams, options?: TransactionOptions) => Promise<boolean>;
+            txData: (params: ITransferFromParams, options?: TransactionOptions) => Promise<string>;
+        };
+        private assign;
+    }
+    export module ERC20 {
+        interface ApprovalEvent {
+            owner: string;
+            spender: string;
+            value: BigNumber;
+            _event: Event;
+        }
+        interface TransferEvent {
+            from: string;
+            to: string;
+            value: BigNumber;
+            _event: Event;
+        }
+    }
+}
+/// <amd-module name="@demo/sdk/contracts/index.ts" />
+declare module "@demo/sdk/contracts/index.ts" {
+    export { ERC20 } from "@demo/sdk/contracts/ERC20.ts";
+}
+/// <amd-module name="@demo/sdk" />
+declare module "@demo/sdk" {
+    import * as Contracts from "@demo/sdk/contracts/index.ts";
+    export default Contracts;
+}
---
+++ packages/eth-contract/test/sdk/types/contracts/ERC20.d.ts
@@ -0,0 +1,85 @@
+import { IWallet, Contract as _Contract, TransactionReceipt, BigNumber, Event, TransactionOptions } from "@ijstech/eth-contract";
+export interface IAllowanceParams {
+    param1: string;
+    param2: string;
+}
+export interface IApproveParams {
+    spender: string;
+    amount: number | BigNumber;
+}
+export interface ITransferParams {
+    recipient: string;
+    amount: number | BigNumber;
+}
+export interface ITransferFromParams {
+    sender: string;
+    recipient: string;
+    amount: number | BigNumber;
+}
+export declare class ERC20 extends _Contract {
+    static _abi: any;
+    constructor(wallet: IWallet, address?: string);
+    deploy(options?: TransactionOptions): Promise<string>;
+    parseApprovalEvent(receipt: TransactionReceipt): ERC20.ApprovalEvent[];
+    decodeApprovalEvent(event: Event): ERC20.ApprovalEvent;
+    parseTransferEvent(receipt: TransactionReceipt): ERC20.TransferEvent[];
+    decodeTransferEvent(event: Event): ERC20.TransferEvent;
+    allowance: {
+        (params: IAllowanceParams, options?: TransactionOptions): Promise<BigNumber>;
+    };
+    approve: {
+        (params: IApproveParams, options?: TransactionOptions): Promise<TransactionReceipt>;
+        call: (params: IApproveParams, options?: TransactionOptions) => Promise<boolean>;
+        txData: (params: IApproveParams, options?: TransactionOptions) => Promise<string>;
+    };
+    balanceOf: {
+        (param1: string, options?: TransactionOptions): Promise<BigNumber>;
+    };
+    burn: {
+        (amount: number | BigNumber, options?: TransactionOptions): Promise<TransactionReceipt>;
+        call: (amount: number | BigNumber, options?: TransactionOptions) => Promise<void>;
+        txData: (amount: number | BigNumber, options?: TransactionOptions) => Promise<string>;
+    };
+    decimals: {
+        (options?: TransactionOptions): Promise<BigNumber>;
+    };
+    mint: {
+        (amount: number | BigNumber, options?: TransactionOptions): Promise<TransactionReceipt>;
+        call: (amount: number | BigNumber, options?: TransactionOptions) => Promise<void>;
+        txData: (amount: number | BigNumber, options?: TransactionOptions) => Promise<string>;
+    };
+    name: {
+        (options?: TransactionOptions): Promise<string>;
+    };
+    symbol: {
+        (options?: TransactionOptions): Promise<string>;
+    };
+    totalSupply: {
+        (options?: TransactionOptions): Promise<BigNumber>;
+    };
+    transfer: {
+        (params: ITransferParams, options?: TransactionOptions): Promise<TransactionReceipt>;
+        call: (params: ITransferParams, options?: TransactionOptions) => Promise<boolean>;
+        txData: (params: ITransferParams, options?: TransactionOptions) => Promise<string>;
+    };
+    transferFrom: {
+        (params: ITransferFromParams, options?: TransactionOptions): Promise<TransactionReceipt>;
+        call: (params: ITransferFromParams, options?: TransactionOptions) => Promise<boolean>;
+        txData: (params: ITransferFromParams, options?: TransactionOptions) => Promise<string>;
+    };
+    private assign;
+}
+export declare module ERC20 {
+    interface ApprovalEvent {
+        owner: string;
+        spender: string;
+        value: BigNumber;
+        _event: Event;
+    }
+    interface TransferEvent {
+        from: string;
+        to: string;
+        value: BigNumber;
+        _event: Event;
+    }
+}
